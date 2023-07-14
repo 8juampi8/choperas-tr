77ca9a0fcd5d1bfd1916dbc46853fbc1bb15b17c
Juan Pablo Girardi <juanpablogirardi07@gmail.com>
2023-07-14 15:48:18 -0300

    Agrego JavaScript a mi página

---
+++ js/stock.js
@@ -0,0 +1,229 @@
+const latas = [
+    {
+        id: 1,
+        nombre: "Summer Ale",
+        precio: 800,
+        tipo: "malta",
+        descripción: "Cerveza dorada, orientada a la malta y de bajo amargor. Refrescante, ligera y con mucha tomabilidad.",
+        cantidad: 1,
+        img:""
+    },
+
+    {
+        id: 2,
+        nombre: "Florida Honey",
+        precio: 800,
+        tipo: "malta",
+        descripción: "Centrada en la malta con una unión armoniosa y sutil de notas a miel, lo que resulta en una cerveza de bajo amargor, bien balanceada y de gran tomabilidad.",
+        cantidad: 1,
+        img:""
+    },
+
+    {
+        id: 3,
+        nombre: "Scottish Export",
+        precio: 800,
+        tipo: "malta",
+        descripción: "Cerveza de color rojizo centrada en la malta, con matices de caramelo en aroma y sabor. Alta tomabilidad y buen cuerpo.",
+        cantidad: 1,
+        img:""
+    },
+
+    {
+        id: 4,
+        nombre: "English Porter",
+        precio: 800,
+        tipo: "malta",
+        descripción: "De característica inglesa, color marrón profundo, casi negro. Cuerpo medio y un perfil de sabores que recuerdan al chocolate y al café.",
+        cantidad: 1,
+        img:""
+    },
+
+    {
+        id: 5,
+        nombre: "Session Ipa",
+        precio: 900,
+        tipo: "lúpulo",
+        descripción: "Cerveza de amargor bajo a moderado. Predomina un intenso sabor y aroma a lúpulos con perfiles americanos. Refrescante, ligera y con mucha tomabilidad.",
+        cantidad: 1,
+        img:""
+    },
+
+    {
+        id: 6,
+        nombre: "American Ipa",
+        precio: 900,
+        tipo: "lúpulo",
+        descripción: "Cerveza muy aromática y sabrosa orientada al lúpulo, de amargor firme pero moderado. Destacan con el sello de los lúpulos americanos, intensas notas cítricas, resinosas y a frutas tropicales. Ligeramente alcohólica y de cuerpo medio.",
+        cantidad: 1,
+        img:""
+    }
+]
+
+const choperaVeinteLitros = [
+    {
+        id: 7,
+        nombre: "Chopera 20 Summer Ale",
+        precio: 16900,
+        tipo: "20L",
+        cantidad: 1,
+        img: ""
+    },
+
+    {
+        id: 8,
+        nombre: "Chopera 20 Florida Honey",
+        precio: 19000,
+        tipo: "20L",
+        cantidad: 1,
+        img: ""
+    },
+
+    {
+        id: 9,
+        nombre: "Chopera 20 Scottish Export",
+        precio: 19000,
+        tipo: "20L",
+        cantidad: 1,
+        img: ""
+    },
+
+    {
+        id: 10,
+        nombre: "Chopera 20 English Porter",
+        precio: 19000,
+        tipo: "20L",
+        cantidad: 1,
+        img: ""
+    },
+
+    {
+        id: 11,
+        nombre: "Chopera 20 Session Ipa",
+        precio: 21400,
+        tipo: "20L",
+        cantidad: 1,
+        img: ""
+    },
+
+    {
+        id: 12,
+        nombre: "Chopera 20 American Ipa",
+        precio: 23800,
+        tipo: "20L",
+        cantidad: 1,
+        img: ""
+    }
+]
+
+const choperaTreintaLitros = [
+    {
+        id: 13,
+        nombre: "Chopera 30 Summer Ale",
+        precio: 25300,
+        tipo: "30L",
+        cantidad: 1,
+        img: ""
+    },
+
+    {
+        id: 14,
+        nombre: "Chopera 30 Florida Honey",
+        precio: 27250,
+        tipo: "30L",
+        cantidad: 1,
+        img: ""
+    },
+
+    {
+        id: 15,
+        nombre: "Chopera 30 Scottish Export",
+        precio: 27250,
+        tipo: "30L",
+        cantidad: 1,
+        img: ""
+    },
+
+    {
+        id: 16,
+        nombre: "Chopera 30 English Porter",
+        precio: 27250,
+        tipo: "30L",
+        cantidad: 1,
+        img: ""
+    },
+
+    {
+        id: 17,
+        nombre: "Chopera 30 Session Ipa",
+        precio: 31950,
+        tipo: "30L",
+        cantidad: 1,
+        img: ""
+    },
+
+    {
+        id: 18,
+        nombre: "Chopera 30 American Ipa",
+        precio: 35450,
+        tipo: "30L",
+        cantidad: 1,
+        img: ""
+    }
+]
+
+const choperaCincuentaLitros = [
+    {
+        id: 19,
+        nombre: "Chopera 50 Summer Ale",
+        precio: 38000,
+        tipo: "50L",
+        cantidad: 1,
+        img: ""
+    },
+
+    {
+        id: 20,
+        nombre: "Chopera 50 Florida Honey",
+        precio: 42300,
+        tipo: "50L",
+        cantidad: 1,
+        img: ""
+    },
+
+    {
+        id: 21,
+        nombre: "Chopera 50 Scottish Export",
+        precio: 42300,
+        tipo: "50L",
+        cantidad: 1,
+        img: ""
+    },
+
+    {
+        id: 22,
+        nombre: "Chopera 50 English Porter",
+        precio: 42300,
+        tipo: "50L",
+        cantidad: 1,
+        img: ""
+    },
+
+    {
+        id: 23,
+        nombre: "Chopera 50 Session Ipa",
+        precio: 47850,
+        tipo: "50L",
+        cantidad: 1,
+        img: ""
+    },
+
+    {
+        id: 24,
+        nombre: "Chopera 50 American Ipa",
+        precio: 53300,
+        tipo: "50L",
+        cantidad: 1,
+        img: ""
+    }
+]
